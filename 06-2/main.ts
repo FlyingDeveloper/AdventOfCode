22d9a3be87a7960e6b8a554c4666f37f653ec07f
Rob Hamilton <RobHamilton+github@gmail.com>
2016-12-18 07:14:40 -0500

    Adding solution for Day 6 part 2

---
+++ 06-2/main.ts
@@ -0,0 +1,51 @@
+import fs = require("fs");
+import _ = require('lodash');
+
+var input = fs.readFileSync("input.txt", { encoding: "utf8" });
+
+class Position {
+    characterCounts = {};
+    constructor() { };
+
+    incrementCharacter(character:string) {
+        if (character.length !== 1) {
+            throw "Input character string must have length of 1";
+        }
+
+        if (!this.characterCounts.hasOwnProperty(character)) {
+            this.characterCounts[character] = 0;
+        }
+
+        this.characterCounts[character]++;
+    }
+
+    getLeastCommonCharacter(): string {
+        var currentMax = Number.MAX_VALUE;
+        var currentMaxCharacter = '';
+        _.forOwn(this.characterCounts, (value:number, key, object) => {
+            if (value < currentMax) {
+                currentMaxCharacter = key;
+                currentMax = value;
+            }
+        });
+
+        return currentMaxCharacter;
+    }
+}
+
+var outputPositions = input.split('\n').reduce((previousValue: Array<Position>, currentValue, currentIndex, array) => {
+    if (currentValue !== '') {
+        currentValue.split('').forEach((character, currentIndex) => {
+            if (previousValue.length <= currentIndex) {
+                previousValue.push(new Position());
+            }
+            previousValue[currentIndex].incrementCharacter(character)
+        });
+    }
+
+    return previousValue;
+}, new Array<Position>());
+
+var output = outputPositions.reduce((accumulator, current, index) => accumulator += current.getLeastCommonCharacter(), '');
+
+console.log(output);
