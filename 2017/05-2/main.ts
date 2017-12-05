28b3da2e26e4ce5297951be4b88bc18545a372bd
Rob Hamilton <RobHamilton+github@gmail.com>
2017-12-05 05:27:37 -0500

    Adding solution for day 5 part 2

---
+++ 2017/05-2/main.ts
@@ -0,0 +1,27 @@
+import * as fs from 'fs';
+
+function processFile(contents: string) {
+    if (contents.endsWith('\n')) {
+        contents = contents.slice(0, contents.length - 1);
+    }
+    const instructions = contents.split('\n').map(Number);
+    let currentPosition = 0;
+    let numberOfMoves = 0;
+    while (currentPosition >= 0 && currentPosition < instructions.length) {
+        let currentMove = instructions[currentPosition];
+        if (currentMove >= 3) {
+            --instructions[currentPosition];
+        } else {
+            ++instructions[currentPosition];
+        }    
+        currentPosition += currentMove;
+        ++numberOfMoves;
+    }
+
+    console.log(numberOfMoves);
+}
+
+/* Test case */
+//processFile('0\n3\n0\n1\n-3\n'); // 10
+
+fs.readFile("input.txt", { encoding: 'utf8' }, (err, contents) => processFile(contents));
